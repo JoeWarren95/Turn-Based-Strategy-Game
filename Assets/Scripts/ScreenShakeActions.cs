3baff8636edd7e57166de953c4b3ef9ded8593bc
JoeWarren95 <jwarren1495@gmail.com>
2023-05-15 19:05:23 -0400

    Basic Grenade VFX added

--- Assets/Scripts/ScreenShakeActions.cs
+++ Assets/Scripts/ScreenShakeActions.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 
 public class ScreenShakeActions : MonoBehaviour
 {
+    //THERE IS AN ERROR, CHECK IT
+
     // Start is called before the first frame update
     void Start()
     {
         ShootAction.OnAnyShoot += ShootAction_OnAnyShoot;
+        GrenadeProjectile.OnAnyGrenadeExploded += GrenadeProjectile_OnAnyGrenadeExploded;
     }
 
+    private void GrenadeProjectile_OnAnyGrenadeExploded(object sender, EventArgs e)
+    {
+        ScreenShake.Instance.Shake(5f);
+    }
+
+    /*private void ShootAction_OnAnyShoot(object sender, ShootAction.OnShootEventArgs e)
+    {
+        ShootAction.OnAnyShoot += ShootAction_OnAnyShoot;
+        //ScreenShake.Instance.Shake();
+    }*/
+
+    //DOUBLE CHECK THIS
     private void ShootAction_OnAnyShoot(object sender, ShootAction.OnShootEventArgs e)
     {
         ScreenShake.Instance.Shake();
     }
-
-    /*private void ShootAction_OnAnyShoot(object sender, ShootAction.OnShootEventArgsEventArgs e)
-    {
-        ScreenShake.Instance.Shake();
-    }*/
 }
